0da6d8d224f65b3845acc1e64bcc91057d1e0c5f
duhd4h <duhd4h@gmail.com>
2021-10-05 01:07:39 +0200

    removed testing farms from code

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -15,35 +15,7 @@ const pools: PoolConfig[] = [
     tokenPerBlock: '10',
     sortOrder: 1,
     isFinished: false,
-  },
-  {
-    sousId: 100,
-    stakingToken: tokens.global,
-    earningToken: tokens.manor,
-    contractAddress: {
-      97: '0x1342d76a2fe7ed435df582a1ff6a618e1f68c18f',
-      56: '',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '100',
-    sortOrder: 2,
-    isFinished: false,
-  },
-  {
-    sousId: 100,
-    stakingToken: tokens.global,
-    earningToken: tokens.manolos,
-    contractAddress: {
-      97: '0xff9071d542f381faa9265a70e4d170e6fb42e026',
-      56: '',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '100',
-    sortOrder: 3,
-    isFinished: false,
-  },
+  }
 ]
 
 export default pools
