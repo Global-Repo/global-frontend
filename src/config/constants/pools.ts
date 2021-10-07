ae9e5db913d3d6ae23407d4d545e5e6b49ba296d
duhd4h <duhd4h@gmail.com>
2021-10-07 02:53:22 +0200

    added new pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -15,6 +15,20 @@ const pools: PoolConfig[] = [
     tokenPerBlock: '10',
     sortOrder: 1,
     isFinished: false,
+  },
+  {
+    sousId: 100,
+    stakingToken: tokens.global,
+    earningToken: tokens.loko,
+    contractAddress: {
+      97: '',
+      56: '0x5E425913EeD548a7C9BfC03A0BB4Aa030B91ce7D',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '1',
+    sortOrder: 2,
+    isFinished: false,
   }
 ]
 
