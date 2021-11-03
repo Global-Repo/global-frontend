4930656a4fab79ad19a2f52440ca3c0f1c88eff5
alvaro <alvaro.heras@simeza.com>
2021-11-03 01:21:08 +0100

    Home Stable

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -289,10 +289,7 @@ const Home: React.FC = () => {
         <img src="/images/home/dots_left.png" alt="global Cube" id="points_in_progress" width="292" height="225"/>
         <img src="/images/home/dots_right.png" alt="global Cube" id="points_in_progress_2" width="292" height="225"/>
         <CustomCountdown>
-          <h1>{t('Whitelist Presale')}</h1>
-
-          <PresaleBuyAmount/>
-          
+          <h1>{t('Whitelist Presale')}</h1>          
           <ProgressBar/>
           <div>
             <p> <b> <span color="black">No</span> {t('Softcap')} </b> </p>
--- src/views/Home/components/PresaleBuyAmount.tsx
+++ src/views/Home/components/PresaleBuyAmount.tsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect,useCallback} from 'react'
-import { ethers } from "ethers";
+import { ethers, utils } from "ethers";
 import { BorderGradientButton, BaseLayout, Flex, Image, LogoIcon, SocialLinks, Text, useMatchBreakpoints, Input, InputProps, Card } from '@duhd4h/global-uikit'
 import styled from 'styled-components'
-import { useWeb3React } from '@web3-react/core'
+import { useWeb3React, Web3ReactProvider  } from '@web3-react/core'
 import Web3 from 'web3'
 import { Web3Provider } from '@ethersproject/providers';
 import UnlockButton from 'components/UnlockButton'
@@ -51,6 +51,12 @@ const Actions = styled.div`
   margin-top: 10px;
 `
 
+export function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 10000
+  return library
+}
+
 
 // anotcationes .--- ya van vendidos x bnb se partira de esa cantidad tendre que llamar para ver 
 const PresaleBuyAmount = () => {
@@ -60,64 +66,62 @@ const PresaleBuyAmount = () => {
   const [pendingTx, setPendingTx] = useState(false)
   const [amount, setAmount] = useState('')
   const { t } = useTranslation()
-  const { library, account } = useWeb3React()
+  const { library, account, active, connector} = useWeb3React()
+
 
   const Buytokens = async () => {
-    console.log("Account" , account);
+
+    console.log("Account" , account, active)
     const contract = getBep20Contract("0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd")
     console.log("Contract", contract)
-    const res = await contract.methods.balanceOf(account).call()
     sendTx(contract,"0xF474Cf03ccEfF28aBc65C9cbaE594F725c80e12d",account,"999999999999994")
+
   }
 
   return (
+     <Web3ReactProvider getLibrary={getLibrary}>
+       <Actions>
 
-         <Actions>
-              {account ? (
-                <ButtonCustomGlobalBuy>
-
-                  <StyledInput
-                    pattern={`^[0-9]*[.,]?[0-9]{0,${18}}$`}
-                    inputMode="decimal"
-                    min="0"
-                    max="25"
-                    placeholder="Amount in BNB"
-                    onChange={event => setAmount(event.target.value)}
-                  />         
-                  <BorderGradientButton
-                    label={
-                      pendingTx
-                        ? t('Buy GLOBAL')
-                        : t('Buy GLOBAL', {
-                        })
-                    }
-                    onClick={async () => {
-                      await Buytokens()
-                    }}
-                    id="harvest-all"
-                    disabled={pendingTx}
-                    width="100%"
-                    style={{ padding: '12px', width: '100%' , height: '40px', background: '#FFECEC', color: '#FF0000', fontSize: '14px', borderRadius: 10}}
-                    colorRight="#FFECEC"
-                    colorLeft="#FFECEC"
-                  />
-
-                </ButtonCustomGlobalBuy>
-
-              ) : (
-                <ButtonCustomGlobalBuy>
-                  <UnlockButton
-                  label={t('Unlock Wallet for buy')}
-                  style={{ padding: '12px', marginTop: '32px', width: '100%' , height: '40px', background: '#FFECEC', color: '#FF0000', fontSize: '14px', borderRadius: 10, border: '1px solid #FFDBDB'}}
-                  width="100%"
-                  colorRight="#FFECEC"
-                  colorLeft="#FFECEC" />
-                </ButtonCustomGlobalBuy>
-              )}
-          </Actions>
-
-
-
+        {account ? (
+          <ButtonCustomGlobalBuy>
+            <StyledInput
+              pattern={`^[0-9]*[.,]?[0-9]{0,${18}}$`}
+              inputMode="decimal"
+              min="0"
+              max="25"
+              placeholder="Amount in BNB"
+              onChange={event => setAmount(event.target.value)}
+            />         
+            <BorderGradientButton
+              label={
+                pendingTx
+                  ? t('Buy GLOBAL')
+                  : t('Buy GLOBAL', {
+                  })
+              }
+              onClick={async () => {
+                await Buytokens()
+              }}
+              id="harvest-all"
+              disabled={pendingTx}
+              width="100%"
+              style={{ padding: '12px', width: '100%' , height: '40px', background: '#FFECEC', color: '#FF0000', fontSize: '14px', borderRadius: 10}}
+              colorRight="#FFECEC"
+              colorLeft="#FFECEC"
+            />
+          </ButtonCustomGlobalBuy>
+        ) : (
+          <ButtonCustomGlobalBuy>
+            <UnlockButton
+            label={t('Unlock Wallet for buy')}
+            style={{ padding: '12px', marginTop: '32px', width: '100%' , height: '40px', background: '#FFECEC', color: '#FF0000', fontSize: '14px', borderRadius: 10, border: '1px solid #FFDBDB'}}
+            width="100%"
+            colorRight="#FFECEC"
+            colorLeft="#FFECEC" />
+          </ButtonCustomGlobalBuy>
+        )}
+        </Actions>
+      </Web3ReactProvider>
   )
 }
 
